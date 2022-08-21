d3c2198c852f88cf9e65de5648a8132009b3f60b
Ashraful Dowla <ashrafuldowlaunited532@gmail.com>
2022-08-21 19:25:03 +0600

    UWF-12 protected route added

---
+++ src/Routes/ProtectedRoute.js
@@ -0,0 +1,26 @@
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+
+const ProtectedRoute = ({ children, isAdmin }) => {
+  const { loading, isAuthenticated, user } = useSelector((state) => state.user);
+
+  if (user == {}) return <Navigate to="/login" />;
+  return (
+    <>
+      {loading === false &&
+        (isAuthenticated === false ? (
+          <Navigate to="/login" />
+        ) : isAdmin ? (
+          user.dashboard.profile.role !== "Admin" ? (
+            <Navigate to="/login" />
+          ) : (
+            children
+          )
+        ) : (
+          children
+        ))}
+    </>
+  );
+};
+
+export default ProtectedRoute;
