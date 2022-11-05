cdbfeff563334b1de112c8ba5fd2fc70a1a9eac0
mituBarua <mitubarua766@gmail.com>
2022-11-05 18:03:18 +0600

    voulunteer and contact page added

--- src/Pages/SubPage/NewsDetailPage/index.js
+++ src/Pages/SubPage/NewsDetailPage/index.js
@@ -31,7 +31,7 @@ const NewsDetails = () => {
       <Banner name="News Details" />
       <div className="container mt-4">
         <div className="row">
-          <div className="col-md-8">
+          <div className="col-md-7">
             <Slider {...settings}>
               {news?.media_list.map(({ url, id }) => (
                 <div className="each-slide" key={id}>
@@ -41,7 +41,7 @@ const NewsDetails = () => {
             </Slider>
           </div>
           <div className="col-md-1"></div>
-                    <div className="col-md-3">
+                    <div className="col-md-4">
                         <div className="categories">
                             <h2>Categories</h2>
                             <ul>
